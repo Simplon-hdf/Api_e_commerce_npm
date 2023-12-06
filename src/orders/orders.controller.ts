7d2e73163c31ed7e441ee200e28de0d3540610af
Thibaud Payen <Thibaudpayenbts@gmail.com>
2023-12-06 19:15:15 +0100

    fix: OrderS et non Order

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -1,26 +1,26 @@
 // src/orders/order.controller.ts
 import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
-import { OrderService } from './orders.service';
+import { OrdersService } from './orders.service';
 import { CreateOrderDTO } from './dto/create-order.dto';
 import { UpdateOrderDTO } from './dto/update-order.dto';
 
 @Controller('orders')
-export class OrderController {
-  constructor(private readonly orderService: OrderService) {}
+export class OrdersController {
+  constructor(private readonly ordersService: OrdersService) {}
 
   @Post()
   create(@Body() createOrderDTO: CreateOrderDTO) {
-    return this.orderService.createOrder(createOrderDTO);
+    return this.ordersService.createOrder(createOrderDTO);
   }
 
   @Get(':orderId')
   findOne(@Param('orderId') orderId: string) {
-    return this.orderService.getOrder(orderId);
+    return this.ordersService.getOrder(orderId);
   }
 
   @Put(':orderId')
   update(@Param('orderId') orderId: string, @Body() updateOrderDTO: UpdateOrderDTO) {
-    return this.orderService.updateOrder(orderId, updateOrderDTO);
+    return this.ordersService.updateOrder(orderId, updateOrderDTO);
   }
 }
 
