1c061420857ce2f2ae113e0f68f6cefbf79480bf
Thibaud Payen <Thibaudpayenbts@gmail.com>
2023-12-07 10:37:56 +0100

    fix: ajout des décorateur Swagger dans create User

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,15 +1,30 @@
 import { IsString, Length, isString } from 'class-validator'
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateUserDto {
-
+    @ApiProperty({
+        description: 'This field represents the new user nickname',
+        minLength: 5,
+        maxLength: 20,
+      })
     @IsString()
     @Length(5, 20)
     pseudo: string;
 
+    @ApiProperty({
+        description: 'This field represents the new user username',
+        minLength: 5,
+        maxLength: 30,
+      })
     @IsString()
     @Length(5, 30)
     username: string;
 
+    @ApiProperty({
+        description: 'This field represents the new user password',
+        minLength: 5,
+        maxLength: 72,
+      })
     @IsString()
     @Length(5, 72)
     password: string;
