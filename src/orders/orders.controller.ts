a23c43af46f8e3f5a48cf1ce5dcc9bff3cbfa322
writememory1337 <ordiplus58@outlook.fr>
2023-12-08 14:07:57 +0100

    feat: added findAll method to return all orders

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -1,8 +1,8 @@
-// src/orders/order.controller.ts
 import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
 import { OrdersService } from './orders.service';
 import { CreateOrderDTO } from './dto/create-order.dto';
 import { UpdateOrderDTO } from './dto/update-order.dto';
+import NormalizedResponse from 'src/utils/normalized-response'; 
 
 @Controller('orders')
 export class OrdersController {
@@ -17,6 +17,10 @@ export class OrdersController {
   findOne(@Param('orderId') orderId: string) {
     return this.ordersService.getOrder(orderId);
   }
+
+  @Get()
+  findAll() {
+    return 'This action returns all orders';  }
 
   @Put(':orderId')
   update(@Param('orderId') orderId: string, @Body() updateOrderDTO: UpdateOrderDTO) {
