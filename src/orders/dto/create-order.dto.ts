aa287cf217e2a60aa6de3329f8a613fbb4d9d806
Thibaud Payen <Thibaudpayenbts@gmail.com>
2023-12-08 14:42:23 +0100

    fix: creater order dto

--- src/orders/dto/create-order.dto.ts
+++ src/orders/dto/create-order.dto.ts
@@ -1,16 +1,5 @@
-// src/orders/dto/create-order.dto.ts
-import { IsArray, IsNotEmpty, IsNumber, IsPositive, IsString } from 'class-validator';
-
 export class CreateOrderDTO {
-  @IsNotEmpty()
-  @IsString()
-  userId: string;
-
-  @IsArray()
-  products: string[]; // Assuming product IDs or details
-
-  @IsNumber()
-  @IsPositive()
-  totalAmount: number;
+  user_uuid : string;
+  product_uuid : string[];
 }
 
