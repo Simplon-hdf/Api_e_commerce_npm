f9cd347555306fff1dfc094c3bde5376365eea1e
Thibaud Payen <Thibaudpayenbts@gmail.com>
2023-12-10 23:43:32 +0100

    fix: ajout de la correction d'order et nouveau schema

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -1,30 +1,24 @@
-import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Delete } from '@nestjs/common';
 import { OrdersService } from './orders.service';
 import { CreateOrderDTO } from './dto/create-order.dto';
-import { UpdateOrderDTO } from './dto/update-order.dto';
-import NormalizedResponse from 'src/utils/normalized-response'; 
 
 @Controller('orders')
 export class OrdersController {
   constructor(private readonly ordersService: OrdersService) {}
 
   @Post()
-  create(@Body() createOrderDTO: CreateOrderDTO) {
-    return this.ordersService.createOrder(createOrderDTO);
+  public create(@Body() createOrderDto: CreateOrderDTO) {
+    return this.ordersService.create(createOrderDto);
   }
 
-  @Get(':orderId')
-  findOne(@Param('orderId') orderId: string) {
-    return this.ordersService.getOrder(orderId);
+  @Get(':id')
+  public getByNumber(@Param('id') id: string) {
+    return this.ordersService.getByNumber(+id);
   }
 
-  @Get()
-  findAll() {
-    return 'This action returns all orders';  }
-
-  @Put(':orderId')
-  update(@Param('orderId') orderId: string, @Body() updateOrderDTO: UpdateOrderDTO) {
-    return this.ordersService.updateOrder(orderId, updateOrderDTO);
+  @Delete(':id')
+  public deleteByNumber(@Param('id') id: string) {
+    return this.ordersService.deleteByNumber(+id);
   }
 }
 
